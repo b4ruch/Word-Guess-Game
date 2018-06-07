4e8f8dbdcd76b7723ffa43a6304c4abed0df7a6d
b4ruch <12469509+b4ruch@users.noreply.github.com>
2018-06-07 04:53:21 -0700

    CSS part 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 /**********************************************
 *Author: Baruch Flores                        *
 *Homework 3: JavaScript Assignment            *
@@ -7,28 +6,43 @@
 ***********************************************/
 
 
-// Global variable that stores total user wins
-var wins = 0;
-
-//Global var that stores total user loses
-var losses = 0;
-
-// var winsHtml = document.getElementById("wins");
-
-//Object game with properties like current word, list of words, guessed words, etc
+//Hangman Object
 var Hangman = {
-    listOfWords: ["doritos", "cheetos", "popcorn", "ruffles", "pringles"],
-    index: 0,
+    listOfWords: ["riesling", "gewurztraminer", "chardonnay", "sauvignon blanc", "syrah", "cabernet sauvignon", "pinot noir", "merlot", "zinfandel"],
     selectedWord: "",
-    currentWord: "",
-    // guessedLetters: [],
-    guessedLetters: ['a', 'b', 'c'],
-    key: "s",
-    lives: 12,
+    currentWord: "",    //word that is being guessed
+    guessedLetters: [],
+    index: 0,           //used to select a word from the list of words
+    key: "",            //key pressed by the user
+    lives: 12,          //remaining user attempts
+    wins: 0,            //total game wins
+    losses: 0,          //total game losses
 
-    // Game Logic here:
-    // If user presses any key,
-    // Check if user had already guessed that letter
+    //gets the length of list of words
+    get length() {
+        return this.listOfWords.length;
+    },
+
+    //Initializes Hangman properties for a new game
+    initialize: function () {
+        this.index = Math.floor(Math.random() * this.length);
+        this.selectedWord = this.listOfWords[this.index];
+        console.log(this.selectedWord);
+        this.currentWord = "";
+        this.currentWord = this.currentWord.padStart((this.selectedWord).length, "-");
+        this.guessedLetters = [];
+        this.key = "";
+        this.lives = 12;
+    },
+
+    //Restarts Hangman to original values (game restart)
+    restart: () => {
+        this.initialize();
+        this.wins = 0;
+        this.losses = 0;
+    },
+
+    // Checks if user had already guessed that letter
     isInGuessedLetters: function () {
         //if letter is guessed, return true, else false
         var list = this.guessedLetters.filter(el => el === this.key.toLowerCase());
@@ -37,13 +51,14 @@ var Hangman = {
         else
             return false;
     },
-    // ->if not,  update the guessed letters
+
+    //Updates guessed letters
     updateGuessedLetters: function () {
         this.guessedLetters.push(this.key);
         document.getElementById("guessedLetters").textContent = this.guessedLetters.toString();
     },
 
-    // returns an array of indexes that matched the key pressed, or empty array if no match
+    //Returns an array of indexes that matched the key pressed, or empty array if no match
     isAMatch: function () {
         var charPos = [];
         var index = 0;
@@ -55,13 +70,11 @@ var Hangman = {
         return charPos;
     },
 
-    // updates currentWord showing the new matched letters
+    //Updates currentWord revealing the new matched letters. Receives the indexes to be revealed
     updateCurrentWord: function (indexArr) {
         indexArr.forEach(element => {
             this.currentWord = this.currentWord.substring(0, element) + this.key + this.currentWord.substring(element + 1);
         });
-
-        document.getElementById("currentWord").textContent = this.currentWord;
     },
 
     //checks if the word is complete by searching any remaining dashes in current word
@@ -71,41 +84,41 @@ var Hangman = {
         else
             return true;
     }
-
 };
 
+// If user presses any key
 document.onkeypress = function (event) {
     Hangman.key = event.key.toLowerCase();
     //if key has not been guessed before
     if (!Hangman.isInGuessedLetters()) {
         Hangman.updateGuessedLetters();
+        document.getElementById("guessedLetters").textContent = Hangman.guessedLetters;
 
         // -> check if the letter matches any character in the selected word
         var indexArr = Hangman.isAMatch();
         if (indexArr.length) {
             // If so, show the letters in current word
             Hangman.updateCurrentWord(indexArr);
-
-            // -> if the word is complete, user wins, game is restarted
+            document.getElementById("currentWord").textContent = Hangman.currentWord;
+            // -> if the word is complete, user wins and game is restarted
             if (Hangman.isWordComplete()) {
-                wins += 1;
-                alert("You are a hero! You saved the Hangman");
-                document.getElementById("wins").textContent = wins;
+                Hangman.wins += 1;
+                document.getElementById("wins").textContent = Hangman.wins;
+                // alert("You are a hero! You saved the Hangman");
                 startGame();
             }
         }
         else {
             //if false, decrease lives by 1 and check if not alive
             if (!--Hangman.lives) {
-                losses++;
-                alert("Ohh no.. hangman is dead!");
-                document.getElementById("losses").textContent = losses;
+                Hangman.losses++;
+                document.getElementById("losses").textContent = Hangman.losses;
+                // alert("Ohh no.. hangman is dead!");
                 startGame();
             }
             else {
                 document.getElementById("guessesRemaining").textContent = Hangman.lives;
             }
-            
         }
     }
     else
@@ -113,31 +126,17 @@ document.onkeypress = function (event) {
 };
 
 
-// -> if no match, discount 1 to the remaining guesses 
-// -> if remaining guesses is less than 1, user loses and game is restarted
-//    (show a message or something to indicate this)
-
 function startGame() {
-    Hangman.index = Math.floor(Math.random() * (Hangman.listOfWords.length));
-    Hangman.selectedWord = Hangman.listOfWords[Hangman.index];
-    console.log(Hangman.selectedWord);
-    Hangman.currentWord = "";
-    Hangman.currentWord = Hangman.currentWord.padStart(Hangman.selectedWord.length, "-");
-    Hangman.guessedLetters = [];
-    Hangman.key = "";
-    Hangman.lives = 12;
+    Hangman.initialize();
     initHtml();
-    // don't forget the function to refresh the HTML values
-    // debugger;
-
 }
 
 
 // Initializes HTML dynamic content
-function initHtml () {
+function initHtml() {
 
-    document.getElementById("wins").textContent = wins;
-    document.getElementById("losses").textContent = losses;
+    document.getElementById("wins").textContent = Hangman.wins;
+    document.getElementById("losses").textContent = Hangman.losses;
     document.getElementById("currentWord").textContent = Hangman.currentWord;
     document.getElementById("guessesRemaining").textContent = Hangman.lives;
     document.getElementById("guessedLetters").textContent = Hangman.guessedLetters;
